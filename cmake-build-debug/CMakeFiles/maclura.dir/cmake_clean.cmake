e6217b3fc65b5cb1e97cee05146a290b3bb1715d
greg <vardygreg23@gmail.com>
2021-06-03 16:09:01 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/maclura.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/maclura.dir/main.cpp.o"
+  "maclura"
+  "maclura.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/maclura.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
